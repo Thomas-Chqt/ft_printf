6ff5fe3b5a95be94e35de789d5eb41c60249ca20
Thomas <tchoquet@student.42tokyo.jp>
2023-06-05 12:05:56 +0900

    update ft_itoa.c

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,76 +6,59 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 18:08:50 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/05/24 16:37:55 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/06/03 13:33:09 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void				absintlen(long nb, unsigned short *len);
 static unsigned short	intlen(int n);
-static void				itoa(int n, char *buffer);
-static void				abs_itoa(long n, unsigned short *i, char *buffer);
 
 char	*ft_itoa(int n)
-{
-	char			*str;	
+{	
+	unsigned int	nbr;
+	unsigned short	n_len;
+	char			*str;
+	unsigned short	i;
 
-	str = malloc((intlen(n) + 1) * sizeof(char));
+	n_len = intlen(n);
+	str = ft_calloc((n_len + 1), sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	itoa(n, str);
+	if (n < 0)
+	{
+		str[0] = '-';
+		nbr = n * -1;
+	}
+	else
+		nbr = n;
+	i = n_len - 1;
+	while ((nbr / 10) > 0)
+	{
+		str[i--] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+	str[i] = (nbr % 10) + '0';
 	return (str);
 }
 
 static unsigned short	intlen(int n)
 {
+	unsigned int	nbr;
 	unsigned short	len;
-	long			l_n;
 
-	len = 0;
-	l_n = (long)n;
-	if (l_n < 0)
+	len = 1;
+	if (n < 0)
 	{
 		len++;
-		l_n *= -1;
+		nbr = n * -1;
 	}
-	absintlen(l_n, &len);
+	else
+		nbr = n;
+	while ((nbr / 10) > 0)
+	{
+		len++;
+		nbr /= 10;
+	}
 	return (len);
-}
-
-static void	absintlen(long nb, unsigned short *len)
-{
-	if ((nb / 10) > 0)
-	{
-		absintlen(nb / 10, len);
-	}
-	(*len)++;
-}
-
-static void	itoa(int n, char *buffer)
-{
-	unsigned short	i;
-	long			l_n;
-
-	i = 0;
-	l_n = (long)n;
-	if (l_n < 0)
-	{
-		buffer[i] = '-';
-		i++;
-		l_n *= -1;
-	}
-	abs_itoa(l_n, &i, buffer);
-	buffer[i] = 0;
-}
-
-static void	abs_itoa(long n, unsigned short *i, char *buffer)
-{
-	if ((n / 10) > 0)
-	{
-		abs_itoa(n / 10, i, buffer);
-	}
-	buffer[*i] = (n % 10) + '0';
-	(*i)++;
 }
